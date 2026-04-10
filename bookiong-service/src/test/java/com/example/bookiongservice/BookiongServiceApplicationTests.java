45b189c077a5da4ad60474d5428399afa6d896d0
wladimirkuhn <wladimirkuhnwork@gmail.com>
2026-04-10 14:14:44 +0300

    Реализована стркутура будущего проекта. Добавлены все необходимые модули и родительский пом файл

---
+++ bookiong-service/src/test/java/com/example/bookiongservice/BookiongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookiongservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookiongServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
